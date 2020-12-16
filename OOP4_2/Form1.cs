346bca0d1319bee26e156e8579d27ecdd03636c8
anyageexy <anya.alexeeva2001@mail.ru>
2020-12-16 18:03:55 +0500

    with_mvs

--- OOP4_2/Form1.cs
+++ OOP4_2/Form1.cs
@@ -12,34 +12,66 @@ namespace OOP4_2
 {
     public partial class Form1 : Form
     {
-        
+        Model model;
         public Form1()
         {
             InitializeComponent();
+            model = new Model();
+            model.observers += new System.EventHandler(this.updateFromModel);
         }
+
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            
-            if (numericUpDown1.Value % 2 == 1) 
+            model.setValue(Decimal.ToInt32(numericUpDown1.Value));
+            /*if (numericUpDown1.Value % 2 == 1) 
                 numericUpDown1.Value = numericUpDown1.Value + 1;//если значение нечетное, прибавить 1
             progressBar1.Value = Decimal.ToInt32(numericUpDown1.Value);
-            textBox1.Text = numericUpDown1.Value.ToString();
+            textBox1.Text = numericUpDown1.Value.ToString();*/
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show(numericUpDown1.Value.ToString());
+            //MessageBox.Show(numericUpDown1.Value.ToString());
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
+
             if (e.KeyCode == Keys.Enter)
             {
-                if(Int32.Parse(textBox1.Text) % 2 == 1)
+                model.setValue(Int32.Parse(textBox1.Text));
+                /*if(Int32.Parse(textBox1.Text) % 2 == 1)
                     textBox1.Text = (Int32.Parse(textBox1.Text) + 1).ToString();
                 numericUpDown1.Value = Int32.Parse(textBox1.Text);
-                progressBar1.Value = Int32.Parse(textBox1.Text); 
+                progressBar1.Value = Int32.Parse(textBox1.Text); */
             }
+        }
+        private void updateFromModel(object sender, EventArgs e)
+        {
+            textBox1.Text = model.getValue().ToString();
+            numericUpDown1.Value = model.getValue();
+            progressBar1.Value = model.getValue();
+        }
+    }
+
+    public class Model
+    {
+        private int value;
+        public System.EventHandler observers;
+        public void setValue(int value)
+        {
+            if (value % 2 == 1)
+                this.value = value + 1;
+            else
+                this.value = value;
+
+            observers.Invoke(this, null);
+        }
+
+        public int getValue()
+        {
+            return value;
         }
     }
 }
